574e2ee60f16dbcddc392c78adc3c0ca48d8f4e1
AndLi <ww1095@163.com>
2015-07-16 21:11:17 +0800

    Initial Commit

---
+++ jstoios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jstoios
+//
+//  Created by AndLi on 15/7/16.
+//  Copyright (c) 2015年 AndLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jstoios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  jstoios
+//
+//  Created by AndLi on 15/7/16.
+//  Copyright (c) 2015年 AndLi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
