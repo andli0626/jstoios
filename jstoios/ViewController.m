02270322769cc0c60d7c6830b83c84ef906533ca
AndLi <ww1095@163.com>
2015-07-17 12:51:41 +0800

    修改标题：增加版本号的显示

--- jstoios/ViewController.m
+++ jstoios/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "NavTitleButton.h"
+
+#import <PgySDK/PgyManager.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *versionLabel;
 
 @end
 
@@ -18,6 +22,55 @@
     [super viewDidLoad];
 
     self.title = @"js to ios";
+    
+    NSDictionary *dic = [[NSBundle mainBundle]infoDictionary];
+    NSString *version = [dic objectForKey:@"CFBundleShortVersionString"];
+
+    _versionLabel.hidden =YES;
+    
+
+    NavTitleButton *titleView = [[NavTitleButton alloc]init];
+    titleView.height = 35;
+    [titleView setTitle:[NSString stringWithFormat:@"jstoios V%@",version] forState:UIControlStateNormal];
+    self.navigationItem.titleView = titleView;
+    
+    [self checkUpdate];
+}
+
+/**
+ *  检查更新
+ */
+- (void)checkUpdate
+{
+    //  有回调的检查更新
+    [[PgyManager sharedPgyManager] checkUpdateWithDelegete:self selector:@selector(updateMethod:)];
+    
+    //    无回调的检查更新，如果有新版本，则会提示用户更新，确认更新后会自动安装新版本
+    //    [[PgyManager sharedPgyManager] checkUpdate];
+}
+
+/**
+ *  检查更新回调
+ *
+ *  @param response 检查更新的返回结果
+ */
+- (void)updateMethod:(NSDictionary *)response
+{
+    if (response[@"downloadURL"]) {
+        
+        NSString *message = response[@"releaseNote"];
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"发现新版本"
+                                                            message:message
+                                                           delegate:self
+                                                  cancelButtonTitle:@"好的"
+                                                  otherButtonTitles:nil,
+                                  nil];
+        
+        [alertView show];
+    }
+    
+    //    调用checkUpdateWithDelegete后可用此方法来更新本地的版本号，如果有更新的话，在调用了此方法后再次调用将不提示更新信息。
+    //    [[PgyManager sharedPgyManager] updateLocalBuildNumber];
 }
 
 @end
